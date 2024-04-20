1a506b9e053fdd19224683dc6d945629b3785583
Peyton11 <peyton.gardner03@gmail.com>
2024-04-20 00:19:01 -0400

    Add UserIOHandler class

--- UserInputHandler.cpp
+++ UserInputHandler.cpp
@@ -3,3 +3,6 @@
 
     Implementation file for UserInputHandler class. Responsible for interacting with the user. Deals with user input/output.
 */
+
+#include "UserInputHandler.hpp"
+
--- UserInputHandler.hpp
+++ UserInputHandler.hpp
@@ -4,3 +4,6 @@
     Interface file for UserInputHandler class. Responsible for interacting with the user. Deals with user input/output.
 */
 
+class UserIOHandler {
+
+};
